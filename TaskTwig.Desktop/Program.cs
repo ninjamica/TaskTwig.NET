04e10e95453c6cbe47b716f91ffa5292d3fadbcd
ninjamike1211 <vandemarkm@gmail.com>
2026-08-15 12:25:51 -0400

    Add wayland support

--- TaskTwig.Desktop/Program.cs
+++ TaskTwig.Desktop/Program.cs
@@ -16,6 +16,7 @@ sealed class Program
     public static AppBuilder BuildAvaloniaApp()
         => AppBuilder.Configure<App>()
             .UsePlatformDetect()
+            .UseWaylandWithFallback()
 #if DEBUG
             .WithDeveloperTools()
 #endif
